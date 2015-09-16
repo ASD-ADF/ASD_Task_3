058752cee51fcaa7f680bab98d242683bbbb4c34
ronauli <adi_smarispat@yahoo.co.id>
2015-09-16 22:47:42 +0700

    produk1(view/search).cpp
    Rona Uli Pardede / 1301144051

--- LinkedList/list.cpp
+++ LinkedList/list.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
 #include "list.h"
 
-void insertafter(list *L, address P, address S) {
+void view(list L)
+{
     address Q;
-    Q=(*L).first;
-    while (info(Q).id!=info(S).id) {
-            Q=next(Q);
-    }
-    next(P)=NULL;
-    next(P)=next(Q);
-    next(Q)=P;
-
-}
-
-void dellast(list *L,address *P) {
-    address Q;
-    Q=(*L).first;
-    while(next(next(Q))!=NULL) {
+    Q=(L).first;
+    while(Q!=NULL)
+    {
+        cout<<"ID        : "<<info(Q).id<<endl;
+        cout<<"Nama      : "<<info(Q).nama<<endl;
+        cout<<"TglMasuk  : "<<info(Q).tgl<<endl;
+        cout<<"Jenis     : "<<info(Q).jenis<<endl;
+        cout<<"Harga     : "<<info(Q).harga<<endl;
+        cout<<endl;
         Q=next(Q);
     }
-    *P=next(Q);
-    next(Q)=NULL;
-    delete P;
+    cout<<"DONE"<<endl;
+}
 
+address searchelm(list L, infotype x){
+    address Q;
+    Q=(L).first;
+    while(info(Q).id!=x.id) {
+        Q=next(Q);
+    }
+    return Q;
 }
