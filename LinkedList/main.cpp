2fdf6732a3d381c7cbf0b73e4b0cb5a6d5a1dca8
jonathangabe <panggabean_jonathan@yahoo.co.id>
2015-09-21 22:24:28 +0700

    Update main.cpp

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -1,9 +1,69 @@
+#include "singlelinklist.h"
 #include <iostream>
 
 using namespace std;
 
+int pil;
 int main()
 {
-    cout << "Hello world!" << endl;
+    infotype x;
+    list l;
+    address p;
+    address q;
+    do
+    {
+        menuapp();
+        if (pil==1)
+        {
+            p=alokasi(x);
+            createlist(&l);
+            inputdata(x);
+            p=alokasi(x);
+            insertfirst(&l,p);
+            cout << " " <<endl;
+
+            inputdata(x);
+            p=alokasi(x);
+            insertfirst(&l,p);
+            cout << " " <<endl;
+            system("cls");
+        }
+        if (pil==2)
+        {
+            inputdata(x);
+            p=alokasi(x);
+            insertlast(&l,p);
+            system("cls");
+        }
+        if (pil==3)
+        {
+            system("cls");
+        }
+        if (pil==4)
+        {
+            deletefirst(&l,p);
+        }
+        if (pil==5)
+        {
+            deletelast(&l,p);
+        }
+        if (pil==6)
+        {
+            deleteafter(q,&p);
+        }
+        if (pil==7)
+        {
+            printinfo(l);
+            getch();
+            system("cls");
+        }
+        if (pil==8)
+        {
+            searchdata(l,x);
+            system("cls");
+        }
+
+    }while (pil!=9);
+
     return 0;
 }
