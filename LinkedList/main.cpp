8b65664e495ab028f7069c12666cc14bc1544354
wendianto <wendianto.96@gmail.com>
2015-09-21 18:30:37 +0700

    Update main.cpp

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -59,42 +59,15 @@ int main()
                 break;
 
         case 4:
-            void deleteFirst (list &l, address p)
-            {
-                if(next(first(l))==nil)
-                {
-                    first(l)=nil;
-                }
-                else
-                {
-                    p=first(l);
-                    first(l)=next(p);
-                    next(p)=nil;
-                }
-            };
+           //Ulya
             break;
 
         case 5:
-            void deleteLast (list &l, address p)
-            {
-                address q;
-                while (next(p)!=nil)
-                {
-                    q=p;
-                    p=next(p);
-                }
-                next(q)=nil;
-                dealokasi(p);
-            };
+         //Ulya
             break;
 
         case 6:
-           void deleteafter(address q, address *p)
-           {
-            next(q)=next(*p);
-            next(*p)=NULL;
-            dealokasi(*p);
-            };
+         //Ulya
             break;
 
         case 7:
