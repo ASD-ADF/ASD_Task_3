7d38a779b3633346c82a756c71652e14795d2b9b
sizoune <muhammadwildaniskandar@gmail.com>
2015-09-21 15:00:13 +0700

    update list.h

--- LinkedList/list.h
+++ LinkedList/list.h
@@ -1,6 +1,54 @@
 #ifndef LIST_H_INCLUDED
 #define LIST_H_INCLUDED
+#include <iostream>
+#include <stdlib.h>
+#include <cstring>
+#include <conio.h>
 
+
+#define nil NULL
+#define info(p) (p)->info
+#define next(p) (p)->next
+#define first(l) ((l).first)
+
+
+struct infotype
+{
+    char id[4];
+    char nama[15];
+    char alamat[25];
+    char jab[10];
+    char hobi[20];
+};
+
+typedef struct elmlist *address;
+
+struct elmlist
+{
+    infotype info;
+    address next;
+};
+
+struct list
+{
+    address first;
+};
+
+void createlist (list *l);
+void inputdata (infotype *x);
+void dealokasi (address p);
+address alokasi (infotype x);
+
+void insertfirst (list *l, address p);
+void insertlast (list *l, address p);
+void insertafter (list *l, address p, address prec);
+
+void deletefirst (list *l, address *p);
+void deletelast (list *l, address *p);
+void deleteafter (list *l, address *p, address prec);
+
+address searchelement(list l, infotype x);
+void viewlist (list l);
 
 
 #endif // LIST_H_INCLUDED
