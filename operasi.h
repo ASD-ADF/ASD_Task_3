b49c6d35e744aa8a55c5992a508cabf29fd048a5
alfinpratama <alfin22gen@gmail.com>
2016-09-23 03:09:13 +0700

    Add files via upload

---
+++ operasi.h
@@ -0,0 +1,16 @@
+#ifndef OPERASI_H_INCLUDED
+#define OPERASI_H_INCLUDED
+
+#include "list.h"
+#include <iostream>
+#include "operasi.h"
+#include <stdio.h>
+using namespace std;
+
+address sentinelSearch (List L, infotype x);
+void insertionSort(List &L);
+
+void deletebyID(List &L, infotype x);
+
+
+#endif // OPERASI_H_INCLUDED
