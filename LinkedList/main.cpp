c96b5fef221e87e12c66a3142f534b7c170128ae
Amin Aenurahman <Shadow47_@msn.com>
2016-09-23 08:26:13 +0700

    mod main

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -19,9 +19,10 @@ int main()
     return 0;
 }
 
-void mainMenu(){
+void mainMenu()
+{
     /**
-	* IS : List sudah dibuat
+    * IS : List sudah dibuat
     * PR : memberikan output pilihan menu kepada user
     *       1. insert first
     *       2. insert after
@@ -34,7 +35,84 @@ void mainMenu(){
     *       9. search
     */
     //-------------your code here-------------
+    while (true)
+    {
+        cout<<" Pilihan Menu    :"<<endl;
+        cout<<"1. Insert First"<<endl;
+        cout<<"2. Insert After"<<endl;
+        cout<<"3. Insert Last"<<endl;
+        cout<<"4. Delete First"<<endl;
+        cout<<"5. Delete After"<<endl;
+        cout<<"6. Delete Last"<<endl;
+        cout<<"7. View List"<<endl;
+        cout<<"8. Insertion sort"<<endl;
+        cout<<"9. Search"<<endl;
+        cout<<"Masukkan Pilihan            : ";
+        cin>>pilihan;
+        switch(pilihan)
+        {
+        case 1:
+            cout<<"Masukkan ID Penjual         : ";
+            cin>>x.id;
+            cout<<"Masukkan Merk Raket         : ";
+            cin>>x.raket;
+            cout<<"Masukkan Harga Raket        : ";
+            cin>>x.harga;
+            P1 = alokasi(x);
+            insertFirst(L, P1);
+            break;
+        case 2:
+            cout<<"Masukkan ID yang sudah ada  : ";
+            cin>>y.id;
+            findElm(L,y);
+            cout<<"Masukkan ID Penjual         : ";
+            cin>>x.id;
+            cout<<"Masukkan Merk Raket         : ";
+            cin>>x.raket;
+            cout<<"Masukkan Harga Raket        : ";
+            cin>>x.harga;
+            P1 = alokasi(x);
+            insertAfter(P1,P2);
+            break;
+        case 3:
+            cout<<"Masukkan ID Penjual         : ";
+            cin>>x.id;
+            cout<<"Masukkan Merk Raket         : ";
+            cin>>x.raket;
+            cout<<"Masukkan Harga Raket        : ";
+            cin>>x.harga;
+            P1 = alokasi(x);
+            insertLast(L, P1);
+            break;
+        case 4:
+            deleteFirst(L,P1);
+            dealokasi(P1);
+            break;
+        case 5:
+            cout<<"Masukkan ID yang akan dihapus: ";
+            cin>>x.id;
+            P1 = findElm(L,x);
+            deleteAfter(P1,P2);
+            printInfo(L);
+            break;
+        case 6 :
+            deleteLast(L,P1);
+            dealokasi(P1);
+            break;
+        case 7:
+            printInfo(L);
+            break;
+        case 8:
+            //insertion sort
+            insertionSort(L);
+            printInfo(L);
+            break;
+        case 9:
+            cout<<"Cari berdasarkan ID          : ";
+            cin>>x.id;
+            sentinelSearch(L,x);
+            printInfo(L);
+            break;
 
-
-    //----------------------------------------
-}
+            //----------------------------------------
+        }
