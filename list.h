e78adc5b749f2182c9e9e4c04b4eebfe8a1e2db4
gumz69 <gumilarsulistian@gmail.com>
2018-02-14 13:11:16 +0700

    1301153625
    All Done

--- list.h
+++ list.h
@@ -24,25 +24,18 @@ using namespace std;
 *
 **/
 
-
-
 typedef mytype infotype;
 typedef struct elmlist *address;
 
 struct elmlist{
-    //------------- your code here -----------
 
-    //----------------------------------------
+    infotype info;
+    address next;
 };
 
 struct List{
-    //------------- your code here -----------
-
-
-    //----------------------------------------
+    address first;
 };
-
-
 
 // define a function and a procedure to allocate and deallocate an element list
 void createList(List &L);
@@ -60,6 +53,5 @@ void deleteAfter(address Prec, address &P);
 // define search-by-ID function and view procedure
 address findElm(List L, infotype x);
 void printInfo(List L);
-
 
 #endif // LIST_H_INCLUDED
