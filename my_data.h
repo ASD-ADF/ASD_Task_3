efe666aa62bf5fc62d933db2709de25d12e43285
vincentiusar <vincentiusarnoldfridolin@gmail.com>
2020-02-19 00:50:59 +0700

    last summary commit
    90% accurate.
    add all .cpp file and .h file

--- my_data.h
+++ my_data.h
@@ -5,9 +5,9 @@
 using namespace std;
 
 /**
-    CLASS      :
-    NAME       :
-    STUDENT ID :
+    CLASS      : IF - 43 - 05
+    NAME       : Vincentius Arnold Fridolin
+    STUDENT ID : 1301190221
 **/
 
 struct mytype {
@@ -20,7 +20,10 @@ struct mytype {
     */
     //=================================================
     // YOUR CODE STARTS HERE
-    your code here
+    int ID;
+    string name;
+    int rank;
+    float score;
 
     // YOUR CODE ENDS HERE
     //=================================================
--- operation.cpp
+++ operation.cpp
@@ -14,9 +14,17 @@ void insertAndSort(List &L, infotype x) {
     */
 
     //-------------your code here-------------
-    your code here
-
-
+    address p, p1;
+    p = first(L);
+    if (p == NULL || info(p).ID > x.ID) {
+        insertFirst(L, allocate(x));
+    } else if (findElm(L, x) == NULL) {
+        while (p != NULL && info(p).ID < x.ID) {
+            p1 = p;
+            p = next(p); 
+        }
+        insertAfter(L, p1, allocate(x));
+    }
     //----------------------------------------
 }
 
@@ -29,8 +37,17 @@ void deletebyID(List &L, int id_x) {
 
     address Prec, P;
     //-------------your code here-------------
-    your code here
-
+    P = first(L);
+    if (P == first(L) && id_x == info(P).ID) {
+        deleteFirst(L, P);
+    } else {
+        while (P != NULL) {
+            if (id_x == info(P).ID && first(L) != last(L)) {
+                deleteAfter(L, prev(P), P);
+            }
+        P = next(P);
+        }
+    }
 
     //----------------------------------------
 }
@@ -43,8 +60,22 @@ void savePassedMember(List &L, List &L2){
     */
     address P;
     //-------------your code here-------------
-    your code here
-
+    P = first(L);
+    address Q = P;
+    while (Q != NULL) {
+        P = Q;
+        if (info(P).score > 80) {
+            insertAndSort(L2, info(P));
+            Q = next(Q);
+            if (prev(P) == NULL) {
+                deleteFirst(L, P);
+            } else {
+                deleteAfter(L, prev(P), P);
+            }
+        } else {
+            Q = next(Q);
+        }
+    }
 
     //----------------------------------------
 }
