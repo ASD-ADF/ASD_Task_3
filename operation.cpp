90341c15b9a15cd55a0ba8959f66427460898d2c
fahrul31 <59928245+fahrul31@users.noreply.github.com>
2020-02-22 18:44:11 +0700

    1301194066 || MUH. FACHRUL HIDAYAT || IF-43-05

--- operation.cpp
+++ operation.cpp
@@ -1,9 +1,11 @@
 #include "list.h"
 #include "operation.h"
 #include "my_data.h"
- 
 
-void insertAndSort(List &L, infotype x) {
+
+
+void insertAndSort(List &L, infotype x)
+{
     /**
     * IS : List may be empty
     * PR : insert a new element into an already sorted-by-ID List L
@@ -14,14 +16,35 @@ void insertAndSort(List &L, infotype x) {
     */
 
     //-------------your code here-------------
-    your code here
-
-
+    address P=L.first;
+    if (L.first==NULL)
+    {
+        insertFirst(L,allocate(x));
+    }
+    else if (P->info.ID > x.ID)
+    {
+        insertFirst(L,allocate(x));
+    }
+    else if (L.last->info.ID < x.ID)
+    {
+        insertLast(L,allocate(x));
+    }
+    else
+    {
+        while (P!=NULL && P->info.ID < x.ID)
+        {
+            P=P->next;
+        }
+        P=P->prev;
+        insertAfter(L,P,allocate(x));
+    }
+    cout<<endl;
     //----------------------------------------
 }
 
 
-void deletebyID(List &L, int id_x) {
+void deletebyID(List &L, int id_x)
+{
     /**
     * IS : List L may be empty
     * FS : an element with ID info = id_x is deleted from List L (deallocate)
@@ -29,22 +52,66 @@ void deletebyID(List &L, int id_x) {
 
     address Prec, P;
     //-------------your code here-------------
-    your code here
-
-
+    P=L.first;
+    if (P->info.ID == id_x)
+    {
+        deleteFirst(L,P);
+        deallocate(P);
+    }
+    else if (L.last->info.ID == id_x)
+    {
+        deleteLast(L,P);
+        deallocate(P);
+    }
+    else
+    {
+        while (P != NULL && P->info.ID != id_x)
+        {
+            P = P->next;
+        }
+        P = P->prev;
+        deleteAfter(L,P,Prec);
+        deallocate(Prec);
+    }
     //----------------------------------------
 }
 
 
-void savePassedMember(List &L, List &L2){
+void savePassedMember(List &L, List &L2)
+{
     /**
     * IS : List L and L2 may be empty
     * FS : any element with score greater than 80 is moved to L2
     */
     address P;
     //-------------your code here-------------
-    your code here
+    address Q=L2.first;
+    P=L.first;
 
+    while (P!=NULL)
+    {
+
+        if (Q==NULL)
+        {
+            if (P->info.score>=80)
+            {
+                insertLast(L2,P);
+            }
+            P=P->next;
+        }
+        else
+        {
+            if (Q->info.ID==P->info.ID)
+            {
+                Q=Q->next;
+            }
+            else if (P->info.score>=80)
+            {
+                insertLast(L2,P);
+            }
+            P=P->next;
+        }
+    }
 
     //----------------------------------------
 }
